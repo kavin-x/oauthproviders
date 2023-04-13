e54bffdb3e4f5ca200e40765494b206899a62133
kavin-x <kavinkumarab@gmail.com>
2023-04-13 20:24:16 +0530

    added exception handling

--- controller/FigmaOauth.js
+++ controller/FigmaOauth.js
@@ -4,7 +4,12 @@ import open from "open";
 dotenv.config();
 
 export const FigmaOauth = () => {
-  open(
-    `https://www.figma.com/oauth?client_id=${process.env.FIGMA_CLIENT_ID}&redirect_uri=http://localhost:9000/oauth/callback&scope=file_read&state=:state&response_type=code`
-  );
+  try {
+    open(
+      `https://www.figma.com/oauth?client_id=${process.env.FIGMA_CLIENT_ID}&redirect_uri=http://localhost:9000/oauth/callback&scope=file_read&state=:state&response_type=code`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  
 };
